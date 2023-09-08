17a4ee4a358c76b5326578fcdcf2e172e08bbd11
Shi.Zongjie <shizongjie@gmail.com>
2023-09-09 00:06:43 +0800

    Update

---
+++ msg_error.py
@@ -0,0 +1,28 @@
+msg_card = {
+        "msg_type": "interactive",
+        "card": {
+            "elements": [
+                {
+                    "tag": "div",
+                    "text": {
+                        "content": "Error during mail parse",
+                        "tag": "lark_md"
+                        }
+                    },
+                {
+                    "tag": "div",
+                    "text": {
+                        "content": "*Login to <a href='https://gmail.com'></a> for more detail information*",
+                        "tag": "lark_md"
+                        }
+                    }
+                ],
+            "header": {
+                "title": {
+                    "content": "{{subject}}",
+                    "tag": "plain_text"
+                    },
+                "template": "{{color}}"
+                }
+            }
+        }

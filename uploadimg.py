cd09a337513c3ce2c624fd540e7434758c38ca3c
Shi.Zongjie <shizongjie@gmail.com>
2023-09-12 01:53:04 +0800

    update

--- uploadimg.py
+++ uploadimg.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 from requests_toolbelt import MultipartEncoder
 
 import feishu
@@ -13,13 +13,21 @@ def upload(imageFile):
         'Authorization': 'Bearer ' + feishu.image_token
     }
     
-    
     multi_form = MultipartEncoder(form)
     
     headers['Content-Type'] = multi_form.content_type
 
     response = requests.request("POST", url, headers=headers, data=multi_form)
     
-    print(response.headers['X-Tt-Logid'])  # for debug or oncall
+    #print(response.headers['X-Tt-Logid'])  # for debug or oncall
     print(response.content)  # Print Response
+
+    image_key = ""
+
+    if response.status_code == 200:
+        raw_data = str(response.text)
+        data = json.loads(raw_data)
+        image_key = data["data"]["image_key"]    
+
+    return image_key
 
